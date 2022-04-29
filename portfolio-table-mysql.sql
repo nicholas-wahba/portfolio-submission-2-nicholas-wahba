94e3fedb47260f7e9cf20ff728f488cb253c3a2e
nicholas-wahba <nicholaswahba@knights.ucf.edu>
2022-04-29 14:51:02 -0400

    main

--- portfolio-table-mysql.sql
+++ portfolio-table-mysql.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE sales;
 
 CREATE TABLE sales.customers (
-	customer_id INT(11),
+	customer_id INT(15),
 	first_name VARCHAR (255),
 	last_name VARCHAR (255),
 	phone VARCHAR (255),
@@ -12,7 +12,6 @@ CREATE TABLE sales.customers (
 	zip_code VARCHAR (255),
     PRIMARY KEY(customer_id)
 );
-
 
 INSERT INTO sales.customers(customer_id, first_name, last_name, phone, email, street, city, state, zip_code) VALUES(0, 'Debra','Burks',NULL,'debra.burks@yahoo.com','9273 Thorne Ave. ','Orchard Park','NY',14127);
 INSERT INTO sales.customers(customer_id, first_name, last_name, phone, email, street, city, state, zip_code) VALUES(1, 'Kasha','Todd',NULL,'kasha.todd@yahoo.com','910 Vine Street ','Campbell','CA',95008);
